5569a7ac380a0458c12ad88eaf4eecdb3cede44f
lazycat <lazycat_develop@163.com>
2018-10-26 15:38:11 +0800

    initialize project

---
+++ process.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import sys
+
+
+class ProcessBar(object):
+
+    def __init__(self, max_steps: int, bar_length=80, done_msg=None):
+        self.max_steps = max_steps
+        self.step = 0
+        self.bar_length = bar_length
+        self.done_msg = done_msg
+
+    def show_process(self):
+
+        if self.step >= self.max_steps - 1:
+            n_arrows = self.bar_length
+            n_lines = 0
+            percent = 100.0
+        else:
+            n_arrows = int(self.step * self.bar_length / self.max_steps)
+            n_lines = self.bar_length - n_arrows
+            percent = self.step * 100.0 / self.max_steps
+
+        bar = '[' + '>' * n_arrows + '-' * n_lines + ']' +\
+              '%.2f' % percent + '%' + '\r'
+
+        sys.stdout.write(bar)
+        sys.stdout.flush()
+
+        self.step += 1
+
+        if self.step >= self.max_steps:
+            print('')
+            if self.done_msg is not None:
+                print(self.done_msg)
